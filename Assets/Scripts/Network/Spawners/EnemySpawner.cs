1f891f158be19bebad0adabf958fd9c49daa2554
Mikael Juhala <mikael.juhala@greenergrass.company>
2023-04-27 16:00:29 +0300

    Add a gizmo for EnemySpawner

--- Assets/Scripts/Network/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Network/Spawners/EnemySpawner.cs
@@ -1,7 +1,27 @@
+using UnityEngine;
+
 public class EnemySpawner : Spawner
 {
 	protected override void OnReady()
 	{
 		MainRpc.Instance.SpawnEnemy(this);
 	}
+
+#if UNITY_EDITOR
+	private void OnDrawGizmos()
+	{
+		using (new TemporaryHandlesMatrix(transform))
+		{
+			using (new TemporaryHandlesColor(Color.red.Opacity(0.03f)))
+			{
+				UnityEditor.Handles.DrawSolidDisc(Vector3.zero, Vector3.up, 0.5f);
+			}
+
+			using (new TemporaryHandlesColor(Color.red))
+			{
+				UnityEditor.Handles.DrawWireDisc(Vector3.zero, Vector3.up, 0.5f);
+			}
+		}
+	}
+#endif
 }
