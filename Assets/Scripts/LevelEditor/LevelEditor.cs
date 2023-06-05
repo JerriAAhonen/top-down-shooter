6bf3b9de4000eeac80df48c728945b5c86bc1b3d
Jerri Ahonen <jerri.ahonen@gmail.com>
2023-06-05 19:08:50 +0300

    Add simple block placing

--- Assets/Scripts/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -10,6 +10,7 @@ public class LevelEditor : MonoBehaviour
 
 	private Grid grid;
 	private Plane mouseInputPlane;
+	private Transform buildGhost;
 
 	private void Awake()
 	{
@@ -19,25 +20,34 @@ public class LevelEditor : MonoBehaviour
 
 	private void Update()
 	{
+		if (buildGhost == null)
+		{
+			buildGhost = Instantiate(testCube);
+		}
+
 		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		if (mouseInputPlane.Raycast(ray, out var distance))
 		{
 			var worldHitPoint = ray.GetPoint(distance);
-			if (Input.GetMouseButtonDown(0))
-				ChangeValue(worldHitPoint);
-
+			
 			if (grid.GetGridAlignedPosition(worldHitPoint, out var gridAlignedWorldPos))
 			{
-				testCube.position = gridAlignedWorldPos;
+				buildGhost.position = gridAlignedWorldPos;
 			}
 			else
-				testCube.position = worldHitPoint;
+				buildGhost.position = worldHitPoint;
+
+			if (Input.GetMouseButtonDown(0))
+				ChangeValue(worldHitPoint);
 		}
     }
 
 	private void ChangeValue(Vector3 worldHitPoint)
 	{
 		if (grid.GetValue(worldHitPoint, out var prev))
+		{
 			grid.SetValue(worldHitPoint, prev + 1);
+			buildGhost = null;
+		}
 	}
 }
